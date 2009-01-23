c1256fdc9a40e19ff6db000b3a4c4641e7c0027c
Marinho Brandão <marinho@note.(none)>
2009-01-23 18:30:01 -0200

    New tests and examples for composition and inheritance

--- geraldo/__init__.py
+++ geraldo/__init__.py
@@ -1,4 +1,37 @@
-VERSION = (0, 1, 'alpha', 2)
+"""
+Geraldo Reports Engine
+======================
+
+Overview
+--------
+
+Geraldo is a reports generator created to work like ReportBuilder, QuickReport
+or Jasper Reports. It has bands with fixed width and flexible height to show on
+top, summary, page header, page footer, in table or each one for an object.
+
+It is under GPL and works only with Django framework and Python language.
+
+It depends on ReportLab library to work.
+
+Packages Structure
+------------------
+
+- base.py - contains report base classes and definitions, including report,
+  subreport, bands an groupping.
+
+- graphics.py - contains graphic classes and definitions
+
+- models.py - there is nothing. Just to be compatible with Django pluggable
+  application structure.
+
+- widgets.py - contains widget classes and definitions.
+
+- generators - a package that contains generator classes.
+
+- tests - a package with automated doc tests.
+"""
+
+VERSION = (0, 1, 'alpha', 3)
 
 def get_version():
     return '%d.%d-%s(%d)'%VERSION
