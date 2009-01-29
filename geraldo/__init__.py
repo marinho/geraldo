3f1e5b4a092f3fc1e9aa3fd4e0993b040d34ced0
Marinho Brandão <marinho@gmail.com>
2009-01-29 16:33:19 -0200

    Little changes to work with PyPI and setuptools

--- geraldo/__init__.py
+++ geraldo/__init__.py
@@ -31,10 +31,10 @@ Packages Structure
 - tests - a package with automated doc tests.
 """
 
-VERSION = (0, 2, 'alpha', 5)
+VERSION = (0, 2, 'stable')
 
 def get_version():
-    return '%d.%d-%s(%d)'%VERSION
+    return '%d.%d-%s'%VERSION
 
 __author__ = 'Marinho Brandao'
 __license__ = 'GNU Lesser General Public License (LGPL)'
--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 # Geraldo setup
 
-from distutils.core import setup
+# Downloads setuptools if not find it before try to import
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup
 from geraldo import get_version
 
 setup(
     name = 'Geraldo',
     version = get_version(),
     description = 'Geraldo is a reports engine for Python and Django applications',
-    long_description = 'Geraldo is a Python and Django pluggable application for that works with ReportLab to generate complex reports.',
+    long_description = 'Geraldo is a Python and Django pluggable application that works with ReportLab to generate complex reports.',
     author = 'Marinho Brandao',
     author_email = 'marinho@gmail.com',
-    url = 'http://github.com/marinho/django-geraldo/',
-    download_url = 'http://github.com/marinho/django-geraldo/',
+    url = 'http://geraldo.sourceforge.net/',
+    download_url = 'http://ufpr.dl.sourceforge.net/sourceforge/geraldo/Geraldo-0.2-stable.tar.gz',
     license = 'GNU Lesser General Public License (LGPL)',
     packages = ['geraldo', 'geraldo.tests', 'geraldo.generators',],
+    install_requires = ['reportlab','pil'],
 )
