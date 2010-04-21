b7b8aec93d6d026658e895e079499c26aebd6fc7
Marinho Brandão <marinho@gmail.com>
2010-04-21 08:05:30 -0300

    Releasing 0.4-final

--- geraldo/__init__.py
+++ geraldo/__init__.py
@@ -19,19 +19,31 @@ Packages Structure
 - base.py - contains report base classes and definitions, including report,
   subreport, bands an groupping.
 
+- barcodes.py - contains code related to render barcodes in reports.
+
+- cache.py - contains settings and backend to store reports in cache.
+
+- charts.py - contains code to make charts.
+
+- cross_reference.py - contains the cross reference table matrix class.
+
+- exceptions.py - contains Geraldo specific exceptions.
+
 - graphics.py - contains graphic classes and definitions
 
 - models.py - there is nothing. Just to be compatible with Django pluggable
-  application structure.
+  application structure and make possible run tests suite.
 
 - widgets.py - contains widget classes and definitions.
+
+- utils.py - contains useful functions, decorators and flags.
 
 - generators - a package that contains generator classes.
 
 - tests - a package with automated doc tests.
 """
 
-VERSION = (0, 4, 'beta')
+VERSION = (0, 4, 'final')
 
 def get_version():
     return '%d.%d-%s'%VERSION
