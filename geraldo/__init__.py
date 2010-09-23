6219ea1e5a186c9a314d6ed32990e2044d2fe240
Marinho Brandão <marinho@gmail.com>
2010-09-23 09:17:33 -0300

    Fixed a test related to ReportGroup.force_new_page

--- geraldo/__init__.py
+++ geraldo/__init__.py
@@ -43,7 +43,7 @@ Packages Structure
 - tests - a package with automated doc tests.
 """
 
-VERSION = (0, 4, 8, 'stable')
+VERSION = (0, 4, 9, 'stable')
 
 def get_version():
     return '%d.%d.%d-%s'%VERSION
